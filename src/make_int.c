1a025a3f8e5146f0ca811fb0ce7cd67fb371fe24
Ruben <rubenxrb@gmail.com>
2017-05-14 03:47:39 +0000

    sagfs

--- src/make_int.c
+++ src/make_int.c
@@ -24,12 +24,17 @@ static t_array	*make_sint(t_agv *fmt, char *lmod, va_list *ap)
 	short	h;
 
 	tmp.d_size = 1;
-	if (!lmod)
+	if (!lmod && !ft_isupper(fmt->type))
 		tmp.data = ft_itoa_base(va_arg(*ap, int), fmt->base);
-	else if (ft_strequ(lmod, "hh") && (hh = va_arg(*ap, int)))
+	else if (!ft_strequ(lmod, "hh") && (hh = va_arg(*ap, int)))
+	{
 		tmp.data = ft_itoa_base(hh, fmt->base);
+	}
 	else if (ft_strequ(lmod, "ll"))
+	{
+		//ft_putendl("testing2");
 		tmp.data = ft_lltoa_base(va_arg(*ap, long long), fmt->base);
+	}
 	else if (*lmod == 'h' && (h = va_arg(*ap, int)))
 		tmp.data = ft_itoa_base(h, fmt->base);
 	else if (*lmod == 'l' || ft_isupper(fmt->type))
