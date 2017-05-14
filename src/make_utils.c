d4a321f5ef514df613307cc3708b1e3c0f7eb960
Ruben <rubenxrb@gmail.com>
2017-05-14 03:11:42 +0000

    actually fixed it lol

--- src/make_utils.c
+++ src/make_utils.c
@@ -6,7 +6,7 @@ t_array	*make_utils(t_agv *fmt, char type, va_list *ap)
 	(void)fmt;
 	(void)type;
 	(void)ap;
-	ret = array_new(sizeof(char), 1);
+	ret = array_new(sizeof(char), 2);
 	if (type == '%')
 		ft_memcpy(ret->data, &type, 1);
 	return ret;
