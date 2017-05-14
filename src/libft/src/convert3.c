d91c246ae572c200031f5661a238096c5c6a230a
Ruben <rubenxrb@gmail.com>
2017-05-14 14:41:20 +0000

    added lib

--- src/libft/src/convert3.c
+++ src/libft/src/convert3.c
@@ -4,19 +4,23 @@
 char	*uint_tostr(unsigned int num, int b)
 {
 	//char	*ret;
-	size_t	size;
+	(void)num;
+	(void)b;
+//	size_t	size;
 
- 	size= ft_numlen(num, b);
-	printf("uint size : %zu\n", size);
+ //	size= ft_numlen(num, b);
+//	printf("uint size : %zu\n", size);
 	return (0);
 }
 
 char	*ulint_tostr(unsigned long long num, int b)
 {
 	//char	*ret;
-	size_t	size;
+	(void)num;
+	(void)b;
+	//size_t	size;
 
- 	size= ft_ulnumlen(num, b);
-	printf("ulint size : %zu\n", size);
+ 	//size= ft_ulnumlen(num, b);
+	//printf("ulint size : %zu\n", size);
 	return (0);
 }
