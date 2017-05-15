e61a6e3d9564be85cf6ba9ea6dbae07a226a0c6f
rubenxrb <rubenxrb@gmail.com>
2017-05-15 08:20:17 +0000

    ksndg

--- src/print_var.c
+++ src/print_var.c
@@ -23,13 +23,13 @@ size_t			print_var(t_array *str, const int fd, const char nxt)
 	if (str->d_size > 1)
 		if (*lol < 0 || (*lol > 55295 && *lol < 57344) || *lol > 1114111)
 		{
-			printf("EXITING\n");
+	//		printf("EXITING\n");
 			exit(-1);
 		}
 //	printf("bytes: '%zu' len: '%zu' d_size: '%zu'\n", str->bytes, str->len, str->d_size);
 	//printf("wchar_len: '%zu'\n", wchar_len(*lol));
-	if (str->d_size > 1)
-		return wstrput_fd(str->data, fd, wchar_len(*lol));
+//	if (str->d_size > 1)
+	//	return wstrput_fd(str->data, fd, wchar_len(*lol));
 //	else if (nxt == 'c' && !*d)
 	//	return (1);
 	return ft_putstr_fd(str->data, fd);
