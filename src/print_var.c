a0e698999d71e5ff6afb03efdb25f3ecbfd82ee6
rubenxrb <rubenxrb@gmail.com>
2017-05-15 08:32:15 +0000

    write would be better than putstring if cause i know the data size im printing

--- src/print_var.c
+++ src/print_var.c
@@ -11,21 +11,22 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdlib.h>
+#include <unistd.h>
 
 size_t			print_var(t_array *str, const int fd, const char nxt)
 {
-	wchar_t	*lol;
+	//wchar_t	*lol;
 
 	(void)nxt;
-	lol = str->data ? str->data : 0;
+	//str->bytes = (nxt == 'c') ? 1 : str->bytes;
+	//lol = str->data ? str->data : 0;
 //	printf("%d\n", *lol);
 //	if (str->d_size > 1)
 	//	if (*lol < 0 || (*lol > 55295 && *lol < 57344) || *lol > 1114111)
 		//{
 	//		printf("EXITING\n");
 			//exit(-1);
-		//}
+//	}
 //	printf("bytes: '%zu' len: '%zu' d_size: '%zu'\n", str->bytes, str->len, str->d_size);
 	//printf("wchar_len: '%zu'\n", wchar_len(*lol));
 //	if (str->d_size > 1)
