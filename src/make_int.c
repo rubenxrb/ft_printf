2b6dec3c1465f6f4fb5821ea3db1cc9d80167da5
rubenxrb <rubenxrb@gmail.com>
2017-05-18 20:18:14 +0000

    asf

--- src/make_int.c
+++ src/make_int.c
@@ -26,8 +26,8 @@ static void	format_integer(t_agv *fmt, t_array **ret, char t)//free old integer
 		(*ret)->bytes = (*ret)->len - 1;
 	//	printf("resized bytes [%zu]\n", (*ret)->bytes);
 		ft_memset((*ret)->data + pl, '0', sp);
-		ft_memcpy((*ret)->data + pl + sp, nbr && *nbr == '-' ? nbr + 1 : nbr,
-		ft_strlen(nbr && *nbr == '-' ? nbr + 1 : nbr));
+		ft_memcpy((*ret)->data + pl + sp, nbr,
+		ft_strlen(nbr));
 	}
 	if (f && *f == ' ' && (*ret)->data)
 		ft_memset((*ret)->data, ' ', 1);
