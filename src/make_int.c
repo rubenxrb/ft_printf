5bce875fa2421ef0748544356e872cbf9311ab23
rubenxrb <rubenxrb@gmail.com>
2017-05-18 19:17:15 +0000

    testing

--- src/make_int.c
+++ src/make_int.c
@@ -36,7 +36,7 @@ static void	format_integer(t_agv *fmt, t_array **ret, char t)//free old integer
 		ft_memset((*ret)->data, (f && *f == '0') ? '0' : ' ', (*ret)->bytes);
 		if (fmt->flgs && fmt->flgs[0] == '-')
 		{
-			ft_memcpy((*ret)->data, nbr, ft_strlen(nbr));
+			ft_memcpy((*ret)->data + pl, nbr, ft_strlen(nbr));
 		}
 		else
 		{
