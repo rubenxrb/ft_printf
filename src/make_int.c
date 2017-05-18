7179d423bb9e98fdb25a185d9db50bb713e6d4ed
rubenxrb <rubenxrb@gmail.com>
2017-05-18 20:13:32 +0000

    lol que mamada

--- src/make_int.c
+++ src/make_int.c
@@ -29,7 +29,7 @@ static void	format_integer(t_agv *fmt, t_array **ret, char t)//free old integer
 		ft_memcpy((*ret)->data + pl + sp, nbr && *nbr == '-' ? nbr + 1 : nbr,
 		ft_strlen(nbr && *nbr == '-' ? nbr + 1 : nbr));
 	}
-	if (*f == ' ' && (*ret)->data)
+	if (f && *f == ' ' && (*ret)->data)
 		ft_memset((*ret)->data, ' ', 1);
 	if (fmt->width > (int)(*ret)->bytes)
 	{
